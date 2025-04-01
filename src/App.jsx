b8382eee473985123da256fee21043798a533363
CristianDanielG <112362318+CristianDanielG@users.noreply.github.com>
2025-04-01 17:04:52 -0500

    attempt to fix spacing by unediting index css

--- src/App.jsx
+++ src/App.jsx
@@ -20,23 +20,34 @@ export function Board() {
   return(
     <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value="1"/>
+      <Square value="2"/>
+      <Square value="3"/>
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
     </div>
     <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+      <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>
     </div>
   </>)
 }
 
-export function Square({ value }){
-  return<button className='square'>{value}</button>
+export function Square({ value }) {
+  function handleClick(){
+    console.log('clicked!');
+  }
+
+  return (
+    <button 
+      className='square'
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
