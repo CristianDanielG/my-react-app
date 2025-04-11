6048af3f55b8f76c20f05d4a6901a4dbc8f7e6f6
CristianDanielG <112362318+CristianDanielG@users.noreply.github.com>
2025-04-10 19:51:16 -0500

    updated Game

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,15 @@ export default function App() {
 export function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
@@ -35,7 +37,7 @@ export function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
